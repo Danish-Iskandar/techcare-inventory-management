1b71e629de51b86b2b59c9d697b272dd2b92372f
shukri1235 <shukriajed@gmail.com>
2024-03-21 00:32:52 +0800

    Add Database file

---
+++ Database/mytechcare.sql
@@ -0,0 +1,156 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mytechcare
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ingredientlist`
+--
+
+DROP TABLE IF EXISTS `ingredientlist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ingredientlist` (
+  `IngredientID` int NOT NULL,
+  `IingredientName` varchar(45) NOT NULL,
+  `IngredientQuantity` int NOT NULL,
+  `IngredientRemarks` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`IngredientID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ingredientlist`
+--
+
+LOCK TABLES `ingredientlist` WRITE;
+/*!40000 ALTER TABLE `ingredientlist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ingredientlist` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orderlist`
+--
+
+DROP TABLE IF EXISTS `orderlist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orderlist` (
+  `OrderID` int NOT NULL,
+  `OrderName` varchar(45) DEFAULT NULL,
+  `OrderMenu` varchar(45) DEFAULT NULL,
+  `Toppings` varchar(45) DEFAULT NULL,
+  `Status` varchar(45) DEFAULT NULL,
+  `Date & Time` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`OrderID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orderlist`
+--
+
+LOCK TABLES `orderlist` WRITE;
+/*!40000 ALTER TABLE `orderlist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orderlist` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `statement`
+--
+
+DROP TABLE IF EXISTS `statement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `statement` (
+  `StatementID` int NOT NULL,
+  `StatementDescription` varchar(1599) NOT NULL,
+  `Out` decimal(7,2) DEFAULT NULL,
+  `In` decimal(7,2) DEFAULT NULL,
+  `Date&Time` datetime(2) NOT NULL,
+  `StatememtRemarks` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`StatementID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `statement`
+--
+
+LOCK TABLES `statement` WRITE;
+/*!40000 ALTER TABLE `statement` DISABLE KEYS */;
+/*!40000 ALTER TABLE `statement` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `userinfo`
+--
+
+DROP TABLE IF EXISTS `userinfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `userinfo` (
+  `idUserInfo` int NOT NULL,
+  `Username` varchar(45) DEFAULT NULL,
+  `Password` varchar(45) DEFAULT NULL,
+  `Admin Access` tinyint NOT NULL,
+  PRIMARY KEY (`idUserInfo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userinfo`
+--
+
+LOCK TABLES `userinfo` WRITE;
+/*!40000 ALTER TABLE `userinfo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `userinfo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `utensillist`
+--
+
+DROP TABLE IF EXISTS `utensillist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `utensillist` (
+  `UtensilID` int NOT NULL,
+  `UtensilName` varchar(45) NOT NULL,
+  `UtensilQuantity` int NOT NULL,
+  `UtensilRemarks` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`UtensilID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utensillist`
+--
+
+LOCK TABLES `utensillist` WRITE;
+/*!40000 ALTER TABLE `utensillist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `utensillist` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-21  0:30:57
