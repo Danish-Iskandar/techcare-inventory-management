f739cb565dbf74af97e58ad4f3736b5159c87f54
Danish Iskandar <danishiskandarzahrul@gmail.com>
2024-03-22 03:33:43 +0800

    Added ease of life features (enter on login, added logo)

--- src/main/java/org/example/inventorymanagement/MainApp.java
+++ src/main/java/org/example/inventorymanagement/MainApp.java
@@ -4,9 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
+import java.awt.*;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class MainApp extends Application {
@@ -14,6 +17,9 @@ public class MainApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("LOGIN.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+
+        Image icon = new Image(new FileInputStream("D:\\Coding\\Java\\javafx\\InventoryManagement\\src\\main\\resources\\PNGs\\TECHCARE-LOGO.png"));
+        stage.getIcons().add(icon);
         stage.setTitle("Inventory Manager");
         stage.setScene(scene);
         stage.setResizable(false);
